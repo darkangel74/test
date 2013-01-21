5b4d8a6543ffa5dd65f30c1e39095e45f11db63f
harits nurrois <d.soul74@yahoo.co.id>
2013-01-21 19:55:55 +0700

    asd
    asd

---
+++ AppWebMVCT3/src/java/action/ActionInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author DARKANGEL
+ */
+public interface ActionInterface {
+    
+   public String execute(HttpServletRequest request);
+   
+}
---
+++ AppWebMVCT3/web/index.jsp
@@ -0,0 +1,1 @@
+<jsp:forward page="/controller?action=home"/>
